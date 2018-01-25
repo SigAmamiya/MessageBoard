8f09241ea8f1da382580a859f62522c26a077d31
Masato Narita <Das_hexenhaus@hotmail.co.jp>
2018-01-25 20:27:51 +0900

    静的なメッセージボードを作成

---
+++ MessageBoard/MessageBoard.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MessageBoard.aspx.cs" Inherits="MessageBoard.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>MessageBoard</title>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+
+
+        <h1>Message Board</h1>
+        <h2>メッセージ一覧</h2>
+                       
+        <p>名前   年齢  内容</p>
+
+        <label>名前：</label>
+        <asp:TextBox ID="NameBox" runat="server" ></asp:TextBox><br />
+
+        <label>年齢：</label>
+        <asp:TextBox ID="AgeBox" runat="server" ></asp:TextBox><br />
+
+        <label>内容：</label>
+        <asp:TextBox ID="TextBox" runat="server" TextMode="MultiLine" Rows="2"></asp:TextBox><br />
+
+        <asp:Button ID="Submit" runat="server" Text="送信" OnClientClick="Submit_Check"  />
+
+    </form>
+</body>
+</html>
---
+++ MessageBoard/MessageBoard.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace MessageBoard {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// NameBox コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NameBox;
+        
+        /// <summary>
+        /// AgeBox コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox AgeBox;
+        
+        /// <summary>
+        /// TextBox コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox;
+        
+        /// <summary>
+        /// Submit コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Submit;
+    }
+}
